a006835faa13cb4ebac4cc05bb67fc4a51a958f5
Saumya Jain <saumya.jain.470@gmail.com>
2020-06-05 18:48:18 -0700

    Adding sequence challenge solution

---
+++ sequencetype_challenge.py
@@ -0,0 +1,18 @@
+from collections import Counter
+
+lst1 = input('Enter string elements separated by space:').split(' ')
+ch=input('Enter the character to find:')
+
+
+distint_list=list(Counter(lst1).keys())
+lst2=[]
+
+for i in range(len(distint_list)):
+    elmnt=distint_list[i]
+    if elmnt.count(ch)>0:
+        for j in range(elmnt.count(ch)):
+            lst2.insert(i,elmnt)
+    else:
+        print(elmnt, ': This element is not in the list')
+
+print(lst2)
